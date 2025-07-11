1d02ad37974fffe8458e08fc9babd8425dd0d349
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2025-07-11 19:54:15 +0700

    Remove button component and update App content
    Deleted the reusable button component and simplified the App component by replacing its content with a static message.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,10 @@
-import { buttonVariants } from "@/components/ui/button";
 
+import React from "react";
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
-      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Vite, React, Shadcn-ui minimal starter
-      </h1>
-      <a
-        href="https://github.com/moinulmoin/vite-react-tailwind-starter"
-        target="_blank"
-        rel="noreferrer"
-        className={buttonVariants()}
-      >
-        ⭐️ on GitHub
-      </a>
-    </main>
+      <div>
+      <p>có cái nịt</p>
+      </div>
   );
 }
 
